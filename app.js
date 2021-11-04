93eb6c8665fda91d078304716620443a9a48caef
qasimmalik8555 <qasimmailk8404@gmail.com>
2021-11-04 07:57:46 -0700

    now complete

---
+++ app.js
@@ -0,0 +1,235 @@
+
+           ////  chapter no one ////
+///question 1///
+
+///var password = " Error! please enter a valid password"///
+///alert(password) ///
+
+///question 2///
+
+///var warning = "wellcome to js land... \n happy coading!"///
+///alert(warning)///
+
+
+/////question 3 ////
+
+
+////var caption = "wellcome to js land..."////
+///alert(caption)///
+
+
+////question 4 ////
+
+////var chek = "happy coading! \n prevent this page from creating this additional dialogs."///
+
+///alert(chek)////
+
+////question 5////
+
+///var hello = "Hello... i can run js through my web browser's console"////
+
+////alert(hello)////
+
+                ///CHAPTER 2///
+
+////question:1/////
+
+// var name = prompt  ("Enter your name")//
+// var age = prompt ("enter your age")//
+// var course = prompt("enter your course name") //
+
+///question:2///
+
+
+////var pizza = "pizza \n "////
+
+////alert(pizza) ////
+
+///question:3////
+
+///var email = "my email addres is :'qasimmalik8404@gmail.com"////
+///alert(email)////
+
+
+
+////question:4///
+
+///var learn = "i am trying to learn from the book A Smarter \n way to Learn Javascript"////
+///alert(learn)///
+
+
+
+///var disply = prompt  ("enter something")///
+
+////document.write( "Yeh! i can write HTML content through JAVASCRIPT" )////
+
+
+ //////////////////////// CHAPTER:3////////////////////////
+
+
+   ////QUESTOIN:1////
+                              
+////var age = "I am 18 year Old"///
+///alert(age)////
+
+
+
+////QUESTION:2////
+
+////var vist = "you have visted this site 14 times"////
+////alert(vist)///////
+
+////QUESTION:3////
+
+ //////////var birth = +prompt ("Enter your date of birth")/////
+ ////document.write( "my date of birth is:31-may-2003" + birth  + "<br>")////
+
+ /////var datatype = prompt ("Enter data type")/////////////
+/////// document.write( " data type of my ddecleared variable is numer" +   datatype  + "<br>")/////
+
+///QUESTION:4///
+
+////var john = prompt ( "enter something" )///////
+/////document.write( "JOHN DOE order 5 T-shirt on XYZ cloathing store" )//////
+
+
+             ///// CHAPTER:4/////
+
+////QUESTION : 1////
+    
+/////var ffd = prompt ( "rules of naming js variables" ) /////
+     
+   //// document.write( "Rules of naming JS veriable  "  )///
+    ////document.write( "veriable name only contain . numer . $ and_ for example:$ my first veriable " )////
+
+                                                  
+                      ////CHAPTER#5///          
+
+    ///QUESTION:1///
+   
+   
+  
+  //   var num1 = 5
+  //   var num2 = 600 
+  // var mul = num1 * num2 
+  // alert(mul)
+  // document.write( " Total cost to buy a 5 ticket of movie is = 3000 PKR" )
+
+
+  /////QUESTION#3////
+
+  // var tablenum = +prompt("Enter table number")
+  // var tablelength = +prompt("Enter table length")
+
+  // for(var i = 1 ; i <=tablelength ; i++){
+
+  //       document.write(tablenum +" x " + i + "="
+  //                          + tablenum*i + "<br>")
+  //}//
+
+
+  ////QUESTION #3///
+
+
+//   var userName = prompt("Enter your name: ");
+
+ 
+//   document.write(  "name: " +  userName  + "<br>")
+ 
+ 
+//   var rollnumber = prompt("Enter your rollnumber: ");
+ 
+  
+//   document.write(  "rollnumber: " + rollnumber  + "<br>")
+ 
+//   var clas = prompt("Enter your class: ");
+ 
+//   document.write( "class: " + clas  + "<br>")
+ 
+ 
+//   var Maths = prompt("Enter your Maths marks: ");
+ 
+  
+//   document.write(  "your Maths mark is: " + Maths  + "<br>")
+ 
+ 
+//   var English = prompt("Enter your English marks: ");
+ 
+  
+//   document.write( "your English mark  is: " + English  + "<br>")
+ 
+ 
+ 
+//   var Urdu = prompt("Enter your URdu marks: ");
+ 
+//   document.write( "your urdu mark is: " + Urdu  + "<br>")
+ 
+//   var Islamait = prompt("Enter your Islamait marks: ");
+ 
+//   document.write( "your islmait mark is: " + Islamait + "<br>")
+ 
+ 
+//   var Pakstudies = prompt("Enter your Pakstudies marks: ");
+ 
+//   document.write( "your pakstudies mark is: " + Pakstudies  + "<br>")
+ 
+ 
+//   var obtaine_marks = parseInt(Maths) + parseInt(English) + parseInt(Urdu) +parseInt(Islamait) +parseInt(Pakstudies)
+  
+//   document.write( "your obtain_mark is: " + obtaine_marks  + "<br>")
+ 
+ 
+//  var percentage = obtaine_marks * 100 / 500
+ 
+//  document.write( "your percentage is: " + percentage  + "<br>")
+ 
+
+
+// question#4
+
+
+//     var num1 = 300
+//    var num2 = 200
+// var num3 = 200        
+//  var add = num1 + num2 + num3 
+//   alert(add)
+  
+//   document.write("Total amount = 700")
+
+
+
+// QUESTION#5
+
+// var name = prompt  ("Enter your name")
+//  var age = prompt ("enter your age")
+//  var course = prompt("ENTER YOUR CURRENT YEAR") 
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
